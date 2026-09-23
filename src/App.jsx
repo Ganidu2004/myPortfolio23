147f74e60357c830a59aadb4b0d67bceb3e927b6
Ganidu2004 <ganiduchalinda@gmail.com>
2026-09-23 21:32:14 +0530

    import the project file in main app💥

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Skill from './components/Skill'
 import Certificates from './components/Certificates'
-import Projects from './components/projects'
+import Projects from './components/Projects'
 import Contact from './components/Contact'
 
 
